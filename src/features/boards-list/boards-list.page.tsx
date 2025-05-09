09a38fd9e81a31cb009fca760d86ff9ed5504684
oleLeo <oldbelom@gmail.com>
2025-05-10 01:04:33 +0300

    feat: api

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,11 +1,18 @@
+import { rqClient } from "@/shared/api/instance";
 import { ROUTES } from "@/shared/model/routes";
 import { Link, href } from "react-router-dom";
 
 function BoardsListPage() {
+    const { data: boardsList } = rqClient.useQuery("get", "/boards");
+
     return (
         <div>
             <h1>Boards list</h1>
-            <Link to={href(ROUTES.BOARD, { boardId: "1" })}>Board 1</Link>
+            {boardsList?.map((board) => (
+                <div key={board.id}>
+                    <Link to={href(ROUTES.BOARD, { boardId: board.id })}>{board.name}</Link>
+                </div>
+            ))}
         </div>
     );
 }
